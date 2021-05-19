00dcd2e02f44d4281bd21fc5e0dfc5e8349c4714
kingaboros <71248858+kingaboros@users.noreply.github.com>
2021-05-19 13:53:47 +0300

    Enhanced Object Literals

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,35 +30,27 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+  // before ES6 we would write:
+  // openingHours: openingHours,
+
+  // ES6 enhanced object literals
+  openingHours,
+
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+  orderDelivery({ starterIndex, mainIndex, time, address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
-  orderPizza: function (mainIng, ...otherIng) {
+  orderPizza(mainIng, ...otherIng) {
     console.log(mainIng);
     console.log(otherIng);
   },
@@ -416,7 +426,6 @@ team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
 
 
-*/
 // Looping Arrays: the FOR-OF loop
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
@@ -426,11 +435,16 @@ for (const item of menu) console.log(item);
 // if we need the index
 
 // for (const item of menu.entries()) {
-//   console.log(`${item[0] + 1}: ${item[1]}`); // this is the old way
-// }
-
-// we can do it better with destructuring elem
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
-// console.log([...menu.entries()]);
+  //   console.log(`${item[0] + 1}: ${item[1]}`); // this is the old way
+  // }
+  
+  // we can do it better with destructuring elem
+  for (const [i, el] of menu.entries()) {
+    console.log(`${i + 1}: ${el}`);
+  }
+  // console.log([...menu.entries()]);
+  
+  
+  // Enhanced Object Literals
+  
+  */
